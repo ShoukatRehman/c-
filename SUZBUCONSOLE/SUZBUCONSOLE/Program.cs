5bf470a3b359aaa475ac2606db30b393013396d6
SHOUKAT <shoukatrahman2001@gmail.com>
2022-12-28 23:06:50 +0500

    simple

---
+++ SUZBUCONSOLE/SUZBUCONSOLE/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SUZBUCONSOLE
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Student abcd = new Student();
+			abcd.Name = "SHOUKAT REHMAN";
+			abcd.ID = 0355;
+			abcd.Mobile = "032899";
+			abcd.ShowDetails(abcd);
+
+			abcd.Name = "akbar ali";
+			abcd.ID = 933;
+			abcd.Mobile = "ouou";
+			abcd.ShowDetails(abcd);
+			
+
+
+			Console.ReadLine();
+
+		}
+	}
+}
---
+++ SUZBUCONSOLE/SUZBUCONSOLE/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUZBUCONSOLE
+{
+	public class Student
+	{
+		public int ID{ get; set; }
+		public string Name { get; set; }
+		public string Mobile { get; set; }
+		public void ShowDetails(Student student)
+		{
+			Console.WriteLine("ID of the student is{0} and Name of the student is {1}\n Mobile Number is {2}", student.ID, student.Name, student.Mobile);
+		}
+	}
+}
